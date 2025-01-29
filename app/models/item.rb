02088e078deb539e048be9b0d76026eb91c7406b
akina0302 <ymu.m24@gmail.com>
2025-01-30 02:01:44 +0900

    商品の登録と一覧ページへの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :user
+  validates :item_name, presence: true
+  validates :price, presence: true,numericality: { greater_thn_or_equal_to:300, less_than_or_equal_to: 9999999}, format: { with:/\A[0-9]+\z/ }
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :payment
+  belongs_to :prefecture
+  belongs_to :derivery_day
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :payment_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :derivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
--- db/migrate/20250127030715_create_items.rb
+++ db/migrate/20250127030715_create_items.rb
@@ -6,7 +6,7 @@ class CreateItems < ActiveRecord::Migration[7.0]
       t.integer :category_id,    null: false
       t.integer :condition_id,   null: false
       t.integer :payment_id,     null: false
-      t.integer :prefacture_id,  null: false
+      t.integer :prefecture_id,  null: false
       t.integer :derivery_day_id,null: false
       t.integer :price,          null: false
       t.references :user,        foreign_key: true , null: false
