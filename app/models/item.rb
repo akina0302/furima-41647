21fa5e3672f7ec76717da8dd985699a6b536953b
akina0302 <ymu.m24@gmail.com>
2025-01-31 01:49:27 +0900

    エラーメッセージの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
+  validates :image, presence: true
   validates :item_name, presence: true
+  validates :description, presence: true
   validates :price, presence: true,numericality: { greater_thn_or_equal_to:300, less_than_or_equal_to: 9999999}, format: { with:/\A[0-9]+\z/ }
   has_one_attached :image
 
@@ -11,10 +13,10 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :derivery_day
 
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :payment_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :derivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, numericality: { other_than: 1 , message: "を入力してください"}
+  validates :condition_id, numericality: { other_than: 1 , message: "を入力してください"}
+  validates :payment_id, numericality: { other_than: 1 , message: "を入力してください"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "を入力してください"}
+  validates :derivery_day_id, numericality: { other_than: 1 , message: "を入力してください"}
 
 end
