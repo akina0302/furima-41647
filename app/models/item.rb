f9c7199e2e625ddb26ae328aaf5eaaac5b0d5c50
akina0302 <ymu.m24@gmail.com>
2025-02-01 01:30:16 +0900

    priceのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :item_name, presence: true,length: { maximum: 40 }
   validates :description, presence: true,length: { maximum: 1000}
-  validates :price, presence: true,numericality: { greater_than_or_equal_to:300, less_than_or_equal_to: 9999999}, format: { with:/\A[0-9]+\z/ }
+  validates :price, presence: true,numericality: { greater_than_or_equal_to:300, less_than_or_equal_to: 9999999, only_integer: true}
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
