2913f64c3b61da6092d2dbe8fa718142110ea09d
akina0302 <ymu.m24@gmail.com>
2025-02-01 01:50:39 +0900

    手数料、販売利益の計算の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,11 +3,12 @@ const price = () => {
 
 const priceInput = document.getElementById("item-price");
 priceInput.addEventListener("input",()=>{
- const inputValue = priceInput.value;
+ const inputValue =Number(priceInput.value);
  const addTaxDom = document.getElementById("add-tax-price");
- addTaxDom.innerHTML = Math.floor(inputValue *0.1)
- const addProfit = document.getElementById("profit")
- addProfit.innerHTML = Math.floor(inputValue-inputValue*0.1)
+ const tax = Math.floor(inputValue *0.1);
+ const addProfit = document.getElementById("profit");
+ addTaxDom.innerHTML = tax;
+ addProfit.innerHTML = inputValue-tax;
 })
 };
 
